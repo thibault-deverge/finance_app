350233a8a4c905736c41aa2498841c40486989dd
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-03-30 00:22:08 +0700

    adding metadata page and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import "@/app/_styles/globals.css";
 
 export const metadata: Metadata = {
-  title: "Personal Finace App",
+  title: {
+    template: "%s / Personal Finance App",
+    default: "Personal Finance App",
+  },
   description: "Track your spending, manage your budgets, and visualize your savings with ease.",
 };
 
@@ -13,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-beige-100 text-grey-900">{children}</body>
+      <body className="min-h-screen bg-beige-100 text-grey-900">{children}</body>
     </html>
   );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from "next";
 import Login from "@/app/_components/Login";
+
+export const metadata: Metadata = {
+  title: "Login",
+  
+};
 
 function Page() {
   return (
-    <section className="flex min-h-screen flex-1 flex-col items-center">
+    <section className="flex  flex-1 flex-col items-center">
       <header className="py-6 bg-grey-900 w-full mb-[10.5rem] rounded-b-lg">
         <img className="mx-auto" src="/images/logo/logo-large.svg" alt="logo finance" width={121} height={22} />
       </header>
