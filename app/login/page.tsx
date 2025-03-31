560b5b4f953b95ff6915eb43d5c7a3e09d8dba09
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-03-31 21:02:51 +0700

    error and notfound page build

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Login from "@/components/Login";
+// import ErrorTest from "./ErrorTest";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -8,6 +9,7 @@ export const metadata: Metadata = {
 function Page() {
   return (
     <section className="flex  flex-1 flex-col items-center">
+      {/* <ErrorTest /> */}
       <header className="py-6 bg-grey-900 w-full mb-[10.5rem] rounded-b-lg">
         <img className="mx-auto" src="/images/logo/logo-large.svg" alt="logo finance" width={121} height={22} />
       </header>
