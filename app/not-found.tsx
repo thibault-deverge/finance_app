31319068db558beab37078441ac80fedd4bd2b1a
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-04-01 19:33:06 +0700

    ajout de la configuration neon et prisma avec modelisation et integration de la bdd local vers une bdd distante heberger sur neon

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <main className="text-center space-y-6 mt-32">
       <h1 className="text-3xl font-semibold">404 - Page Not Found</h1>
       <p className="text-lg">The page you are looking for doesn't exist.</p>
-      <a href="/" className="inline-block bg-grey-900 px-6 py-3 text-lg text-beige-100">
+      <Link href="/" className="inline-block bg-grey-900 px-6 py-3 text-lg text-beige-100">
         Back to Home
-      </a>
+      </Link>
     </main>
   );
 }
